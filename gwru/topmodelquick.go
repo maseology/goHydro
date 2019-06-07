499dc7c9794489a8b55c5c16392ac4237392c982
maseology <maseology@pm.me>
2019-06-07 11:03:19 -0400

    added TMQ comparer

--- gwru/topmodelquick.go
+++ gwru/topmodelquick.go
@@ -1,7 +1,10 @@
 package gwru
 
 import (
+	"fmt"
 	"math"
+
+	"github.com/maseology/mmaths"
 )
 
 // TMQ is an optimized variation of the TOPMODEL struct
@@ -22,4 +25,34 @@ func (t *TMQ) Update(g float64) float64 {
 // GetDi returns the local subsurface deficit (Di)
 func (t *TMQ) GetDi(cid int) float64 {
 	return t.Dm + t.t[cid]
+}
+
+// IsSame compares two TMQ structs
+func (t *TMQ) IsSame(t1 *TMQ) (bool, string) {
+	if t.M != t1.M {
+		return false, "m"
+	}
+	if t.Qo != t1.Qo {
+		return false, "Qo"
+	}
+	if t.Dm != t1.Dm {
+		return false, "Dm"
+	}
+	if t.ca != t1.ca {
+		return false, "ca"
+	}
+
+	c := 0
+	for i, t := range t.t {
+		tt := t1.t[i]
+		rd := math.Abs(mmaths.RelativeDifference(t, tt))
+		if rd > 1e-10 {
+			c++
+		}
+	}
+	if c > 0 {
+		return false, fmt.Sprintf("t: %d", c)
+	}
+
+	return true, "nil"
 }
