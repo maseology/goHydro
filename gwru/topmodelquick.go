c124f3772a25a2b124436ccbf4085b8818c41909
maseology <mason@riffle.ca>
2019-09-25 13:40:14 -0400

    reduced topmodelquick

--- gwru/topmodelquick.go
+++ gwru/topmodelquick.go
@@ -1,32 +1,77 @@
 package gwru
 
 import (
-	"fmt"
-	"math"
 	"log"
+	"math"
 
-	"github.com/maseology/mmaths"
+	"github.com/maseology/goHydro/tem"
 	"github.com/maseology/mmio"
 )
 
 const (
-	omega   = 1. // sinuosity
+	omega = 1. // sinuosity
 )
 
 // TMQ is an optimized, distributed variation of the TOPMODEL struct
 type TMQ struct {
-	D, Qs     map[int]float64 // cell deficit relative to Dm; saturated lateral discharge (=w To tan(beta)/a)
-	Dm, M, Ca float64         // mean deficit, discharge at Dm=0, Prameter m, catchment area
+	D, Qs map[int]float64 // cell deficit relative to Dm; saturated lateral discharge (=w To tan(beta)/a)
+	M     float64
 }
 
 // Copy TMQ
 func (t *TMQ) Copy() TMQ {
 	return TMQ{
 		D:  mmio.CopyMapif(t.D),
-		Dm: t.Dm,
-		M:  t.M,
-		Ca: t.Ca,
+		Qs: mmio.CopyMapif(t.Qs),
 	}
+}
+
+// New constructor. unit-volum inputs (i.e., [m/ts])
+//  ksat: saturated hydraulic conductivity [m/ts]
+func (t *TMQ) New(ksat map[int]float64, strm []int, topo *tem.TEM, cw, m float64) (map[int]float64, float64) {
+	t.M = m
+	n := len(ksat)                          // number of cells
+	t.Qs = make(map[int]float64, len(strm)) // saturated lateral discharge [m/ts]
+	for _, c := range strm {
+		if _, ok := ksat[c]; ok {
+			t.Qs[c] = 0.
+		}
+	}
+
+	// compute unit contributing areas
+	uca := make(map[int]int, n)
+	for i := range ksat {
+		uca[i] = 1
+		for _, c := range topo.UpIDs(i) {
+			if _, ok := ksat[c]; ok { // to be kept within sws
+				uca[i] += topo.UnitContributingArea(c)
+			}
+		}
+	}
+
+	g := 0.
+	ti := make(map[int]float64, n) // soil-topographic index
+	t.D = make(map[int]float64, n) // cell deficits relative to Dm
+	for i, k := range ksat {
+		tsat := k * cw                        // lateral transmisivity when soil is saturated [m²/ts]
+		tanbeta := math.Tan(topo.TEC[i].S)    // gradient
+		ai := float64(uca[i]) * cw            // contributing area per unit contour [m] (assumes uniform square cells)
+		ti[i] = math.Log(ai / tsat / tanbeta) // soil-topographic index
+		if math.IsNaN(ti[i]) {
+			log.Fatalf(" TMQ.New error: topographic index is NaN. slope = %f\n", topo.TEC[i].S)
+		}
+		if _, ok := t.Qs[i]; ok {
+			t.Qs[i] = omega * tsat * tanbeta / cw // (Qi) saturated lateral discharge (when Dm=0) at stream cells [m/ts]
+		}
+		g += ti[i] // gamma
+	}
+	g /= float64(n) // assumes uniform square cells
+
+	for i, v := range ti {
+		t.D[i] = m * (g - v) // deficit at cell i relative to Dm [m]
+	}
+
+	return ti, g
 }
 
 // RelTi returns the topographical index relative to gamma (g-Ti)
@@ -36,61 +81,4 @@ func (t *TMQ) RelTi() map[int]float64 {
 		out[k] = v / t.M
 	}
 	return out
-}
-
-// // Clone creates a deep copy of TMQ, while changing recession coefficient m
-// func (t *TMQ) Clone(m float64) TMQ {
-// 	dnew := make(map[int]float64, len(t.d))
-// 	for i, v := range t.d {
-// 		dnew[i] = m * v / t.M
-// 	}
-// 	return TMQ{
-// 		d:  dnew,
-// 		Dm: 0.,
-// 		M:  m,
-// 		Ca: t.Ca,
-// 	}
-// }
-
-// GetDi returns the local subsurface deficit (Di)
-func (t *TMQ) GetDi(cid int) float64 {
-	if d, ok := t.D[cid]; ok {
-		return t.Dm + d
-	}
-	log.Fatalf(" TMQ.GetDi error: cell id %d not found in model\n", cid)
-	return t.Dm 
-}
-
-// // UpdateLumped state. input g: total basin average recharge per time step [m]
-// func (t *TMQ) UpdateLumped(g float64) float64 {
-// 	qb := t.Qo * math.Exp(-t.Dm/t.M) // gw discharge to streams [m/ts]
-// 	t.Dm -= g                        // add recharge [m/ts]
-// 	t.Dm += qb                       // remove baseflow discharge
-// 	return qb                        // baseflow [m/ts]
-// }
-
-// IsSame compares two TMQ structs
-func (t *TMQ) IsSame(t1 *TMQ) (bool, string) {
-	if t.M != t1.M {
-		return false, "m"
-	}
-	if t.Dm != t1.Dm {
-		return false, "Dm"
-	}
-	if t.Ca != t1.Ca {
-		return false, "ca"
-	}
-
-	c := 0
-	for i, d := range t.D {
-		dd := t1.D[i]
-		rd := math.Abs(mmaths.RelativeDifference(d, dd))
-		if rd > 1e-10 {
-			c++
-		}
-	}
-	if c > 0 {
-		return false, fmt.Sprintf("t: %d", c)
-	}
-	return true, "nil"
 }
