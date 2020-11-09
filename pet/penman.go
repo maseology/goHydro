2c4657c238630fee1ec2c001304777afb4260ea6
maseology <mason@riffle.ca>
2020-11-09 16:38:08 -0500

    update to penmanWind

--- pet/penman.go
+++ pet/penman.go
@@ -26,10 +26,18 @@ func Penman(Rn, t, p, rh, u, a, b float64) (float64, float64) {
 // PenmanWind mass density flux m/s ~m³/m²/s
 // Penman (1948) based solely on the wind function
 func PenmanWind(t, p, rh, u, a, b float64) float64 {
-	m, g := slopeSaturationCurve(t), psychrometricConstant(t, p) // [Pa/°C],[Pa/°C]
-	pa, pw := densityDryAir(t)/1000., densityLiquidWater(t)      // [kg/m³]      // pa := densityMoistAir(t, rh)/1000.
+	// m, g := slopeSaturationCurve(t), psychrometricConstant(t, p) // [Pa/°C],[Pa/°C]
+	// pa, pw := densityDryAir(t)/1000., densityLiquidWater(t)      // [kg/m³]      // pa := densityMoistAir(t, rh)/1000.
 
-	de := vapourPressureDeficit(t, rh)                  // [Pa]
-	Dv := windFunction(u, a, b)                         // [m/s]
-	return (1. - m/(m+g)) * pa * mwr / p * de * Dv / pw // [m/s]
+	// de := vapourPressureDeficit(t, rh)                  // [Pa]
+	// Dv := windFunction(u, a, b)                         // [m/s]
+	// return (1. - m/(m+g)) * pa * mwr / p * de * Dv / pw // [m/s]
+
+	m, g, l := slopeSaturationCurve(t), psychrometricConstant(t, p), latenHeatVapouration(t) // [Pa/°C],[Pa/°C],[MJ/kg]
+	pa, pw := densityDryAir(t), densityLiquidWater(t)                                        // [kg/m³],[kg/m³]      // pa := densityMoistAir(t, rh)/1000.
+	l *= 1.e6 * pw                                                                           // [W·s/m³]
+
+	de := vapourPressureDeficit(t, rh)     // [Pa]
+	Dv := windFunction(u, a, b)            // [m/s]
+	return pa * ca * de * Dv / l / (m + g) // [m/s]
 }
