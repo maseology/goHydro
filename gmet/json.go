683704cff20568367a7d5f647cb68ead3a54bb88
maseology <mason@riffle.ca>
2021-06-11 16:29:01 -0400

    update

---
+++ gmet/json.go
@@ -0,0 +1,83 @@
+package gmet
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func toJson(ds []dset) string {
+	var iv interface{}
+	iv = ds
+
+	jsonMsg, err := json.Marshal(iv)
+	if err != nil {
+		return fmt.Sprintf("ERROR (GMET.pointToJson): %v", err)
+	}
+	return string(jsonMsg)
+}
+
+func (g *GMET) StaToJson(xsid int) string {
+	// library(ggplot2)
+	// library(jsonlite)
+	// library(dplyr)
+
+	// df <- fromJSON("E:/YCDBnetCDF4.gob.json") %>% na_if(-999)
+	// View(df)
+
+	// ggplot(df,aes(as.Date(Date))) +
+	//   geom_line(aes(y=Tx)) +
+	//   geom_line(aes(y=Tn))
+
+	return toJson(g.Dat[xsid])
+}
+
+func (g *GMET) StasToJson(xsids []int) string {
+
+	ds := make([]dset, len(g.Ts))
+	div0 := func(n, d float64) float64 {
+		if d != 0. {
+			return n / d
+		}
+		return -999.
+	}
+	for i, t := range g.Ts {
+		sum, cnt := dset{}, dset{}
+		for _, j := range xsids {
+			if v := g.Dat[j][i].Tx; v > -100. {
+				sum.Tx += v
+				cnt.Tx++
+			}
+			if v := g.Dat[j][i].Tn; v > -100. {
+				sum.Tn += v
+				cnt.Tn++
+			}
+			if v := g.Dat[j][i].Rf; v > -100. {
+				sum.Rf += v
+				cnt.Rf++
+			}
+			if v := g.Dat[j][i].Sf; v > -100. {
+				sum.Sf += v
+				cnt.Sf++
+			}
+			if v := g.Dat[j][i].Sd; v > -100. {
+				sum.Sd += v
+				cnt.Sd++
+			}
+			if v := g.Dat[j][i].Pa; v > -100. {
+				sum.Pa += v
+				cnt.Pa++
+			}
+		}
+		ds[i] = dset{
+			Date: t.Format("2006-01-02"),
+			Tx:   div0(sum.Tx, cnt.Tx),
+			Tn:   div0(sum.Tn, cnt.Tn),
+			Rf:   div0(sum.Rf, cnt.Rf),
+			Sf:   div0(sum.Sf, cnt.Sf),
+			Sd:   div0(sum.Sd, cnt.Sd),
+			Pa:   div0(sum.Pa, cnt.Pa),
+		}
+	}
+
+	return toJson(ds)
+}
---
+++ grid/sws.go
@@ -0,0 +1,86 @@
+package grid
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/maseology/mmio"
+)
+
+type SWS struct {
+	GD         *Definition
+	SwsC, Usws map[int][]int // SwsC: [swsID]cellID; Usws: [downstream_swsID]upstream_swsID
+	CSws, Dsws map[int]int   // CSws: [cellID]swsID; Dsws: [upstream_swsID]downstream_swsID
+}
+
+func CollectSWS(swsFP string, gd *Definition) *SWS {
+
+	var gsws Indx
+	gsws.LoadGDef(gd)
+	gsws.New(swsFP, false)
+	cs := gsws.Values()
+	sc := make(map[int][]int, len(gsws.UniqueValues()))
+	for c, s := range cs {
+		if _, ok := sc[s]; ok {
+			sc[s] = append(sc[s], c)
+		} else {
+			sc[s] = []int{c}
+		}
+	}
+
+	// collect topology
+	var dsws map[int]int
+	var usws map[int][]int
+	topoFP := mmio.RemoveExtension(swsFP) + ".topo"
+	nsws := len(sc)
+	if _, ok := mmio.FileExists(topoFP); ok {
+		d, err := mmio.ReadCSV(topoFP)
+		if err != nil {
+			log.Fatalf(" Loader.readSWS: error reading %s: %v\n", topoFP, err)
+		}
+		// dsws = make(map[int]int, len(d)) // note: swsids not contained within dsws drain to farfield
+		// for _, ln := range d {
+		// 	dsws[int(ln[1])] = int(ln[2]) // linkID,upstream_swsID,downstream_swsID
+		// }
+		dsws = make(map[int]int, nsws) // note: swsids not contained within dsws drain to farfield
+		usws = make(map[int][]int, nsws)
+		for _, ln := range d {
+			if _, ok := sc[int(ln[1])]; ok {
+				if _, ok := sc[int(ln[2])]; ok {
+					dsws[int(ln[1])] = int(ln[2]) // linkID,upstream_swsID,downstream_swsID
+					usws[int(ln[2])] = append(usws[int(ln[2])], int(ln[1]))
+				}
+			}
+		}
+	} else {
+		// fmt.Printf(" warning: sws topology (*.topo) not found\n")
+		log.Fatalf(" BuildRTR error: sws topology (*.topo) not found: %s", topoFP)
+	}
+
+	return &SWS{
+		GD:   gd,
+		CSws: cs,
+		SwsC: sc,
+		Dsws: dsws,
+		Usws: usws,
+	}
+}
+
+func (s *SWS) ClimbFrom(swsID int) []int {
+	var i []int
+	var recurs func(int)
+	recurs = func(sid int) {
+		i = append(i, sid)
+		if a, ok := s.Usws[sid]; ok {
+			for _, ii := range a {
+				recurs(ii)
+			}
+		}
+	}
+	recurs(swsID)
+	return i
+}
+
+func (s *SWS) Print() {
+	fmt.Println(len(s.CSws), len(s.SwsC), len(s.Dsws), len(s.Usws))
+}
