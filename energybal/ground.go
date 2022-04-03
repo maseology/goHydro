b303558d1072732a8a5f8f72c4b99f69523e928d
maseology <maseology@pm.me>
2022-04-03 15:46:13 -0400

    clean up code

--- energybal/ground.go
+++ energybal/ground.go
@@ -3,7 +3,7 @@ package energybal
 // GroundHeatFlux [W/m²]
 // kg: thermal conductivity of soil [W/m/K] (pg.171 DeWalle & Rango)  .2-2. W/m/K (Oke, 1987)
 // ts: soil temperature [°C] measured at depth zts
-func GroundHeatFlux(tC, ts, zts, kg float64) float64 {
-	// if snow present, tC>=0.
-	return kg * (tC - ts) / zts
+func GroundHeatFlux(Tair, ts, zts, kg float64) float64 {
+	// if snow present, Tair>=0.
+	return kg * (Tair - ts) / zts
 }
